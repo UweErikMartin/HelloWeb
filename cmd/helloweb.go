c2ce39883154571a4f037cb300f45526e789d7c4
Uwe-Erik Martin <uwe_erik_martin@yahoo.de>
2023-09-22 20:26:59 +0200

    minimal web service

--- cmd/helloweb.go
+++ cmd/helloweb.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"k8s.io/klog"
 )
 
 func main() {
-	fmt.Println("Hello Web!")
-	klog.Info("Hello Web!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", helloWeb)
+
+	klog.Infoln("Start to listen on port 80")
+	err := http.ListenAndServe(":80", mux)
+	if err != nil {
+		klog.Errorln(err)
+	}
+}
+
+func helloWeb(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Web!")
 }
