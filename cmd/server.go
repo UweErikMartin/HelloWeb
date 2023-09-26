f2b5bd5447df5b641c8fb06c4a57907e15659ec7
Uwe-Erik.Martin <uwe-erik.martin@siemens-healthineers.com>
2023-09-26 17:37:48 +0200

    fix memory leak

--- cmd/server.go
+++ cmd/server.go
@@ -11,39 +11,33 @@ import (
 	"k8s.io/klog"
 )
 
+var memStats runtime.MemStats
+
 func main() {
-	var port int
-	var rootUrl string
+	var intPort int
+	var strRootUrl string
 
 	// Read the commandline and environment variables into the application config
 	var flags = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	flags.IntVar(&port, "port", 80, "port the server is listening on")
-	flags.StringVar(&rootUrl, "root-url", "/", "root url the server is serving")
+	flags.IntVar(&intPort, "port", 80, "port the server is listening on")
+	flags.StringVar(&strRootUrl, "root-url", "", "root url the server is serving")
 	flags.Parse(os.Args[1:])
 	flagutil.SetFlagsFromEnv(flags, "SERVER")
 
-	defer klog.Flush()
+	strPort := fmt.Sprintf(":%d", intPort)
+	strPath := fmt.Sprintf("%s/health", strRootUrl)
+	runtime.ReadMemStats(&memStats)
 
-	// Start health endpoint
-	addr := fmt.Sprintf(":%d", port)
-	healthEndPoint := http.NewServeMux()
+	mux := http.NewServeMux()
 
-	healthEndPoint.HandleFunc(rootUrl+"/health", healthProbeHandler)
-	klog.Infof("Listening on :%d%s\n", port, rootUrl)
-
-	klog.Fatal(http.ListenAndServe(addr, healthEndPoint))
+	mux.HandleFunc(strPath, handleHealth)
+	klog.Infof("Listening on %s%s - MemoryAllocated: %d\n", strPort, strPath, memStats.TotalAlloc)
+	klog.Fatal(http.ListenAndServe(strPort, mux))
 }
 
-func healthProbeHandler(w http.ResponseWriter, r *http.Request) {
-	hn, err := os.Hostname()
-
-	if err != nil {
-		klog.Errorln(err)
-	}
-
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-
-	klog.Infof("serving health for %s - MemoryAllocated: %d\n", r.RemoteAddr, m.TotalAlloc)
-	fmt.Fprint(w, hn+" feels well!")
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	msg := fmt.Sprintf("MemoryAllocated: %d\n", memStats.TotalAlloc)
+	hn, _ := os.Hostname()
+	klog.Infof(msg)
+	fmt.Fprintf(w, "%s feels well %s", hn, msg)
 }
