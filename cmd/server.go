4d301031354b324b1eebba4bca3dbe0a0fca4aa0
Uwe-Erik.Martin <uwe-erik.martin@siemens-healthineers.com>
2023-09-27 10:05:54 +0200

    add host parameter

--- cmd/server.go
+++ cmd/server.go
@@ -15,23 +15,25 @@ var memStats runtime.MemStats
 
 func main() {
 	var intPort int
-	var strRootUrl string
+	var strPath string
+	var strHost string
 
 	// Read the commandline and environment variables into the application config
 	var flags = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	flags.IntVar(&intPort, "port", 80, "port the server is listening on")
-	flags.StringVar(&strRootUrl, "root-url", "", "root url the server is serving")
+	flags.StringVar(&strPath, "path", "", "path the server is serving")
+	flags.StringVar(&strHost, "host", "", "host the server is serving")
 	flags.Parse(os.Args[1:])
 	flagutil.SetFlagsFromEnv(flags, "SERVER")
 
-	strPort := fmt.Sprintf(":%d", intPort)
-	strPath := fmt.Sprintf("%s/health", strRootUrl)
+	strPort := fmt.Sprintf("%s:%d", strHost, intPort)
+	strHandlerPath := fmt.Sprintf("%s/health", strPath)
 	runtime.ReadMemStats(&memStats)
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc(strPath, handleHealth)
-	klog.Infof("Listening on %s%s - MemoryAllocated: %d\n", strPort, strPath, memStats.TotalAlloc)
+	mux.HandleFunc(strHandlerPath, handleHealth)
+	klog.Infof("Listening on %s%s - MemoryAllocated: %d\n", strPort, strHandlerPath, memStats.TotalAlloc)
 	klog.Fatal(http.ListenAndServe(strPort, mux))
 }
 
