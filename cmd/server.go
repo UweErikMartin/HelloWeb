6a72f27f48232f9ccbc8e26b6a61f072c109cf4a
Uwe-Erik.Martin <uwe-erik.martin@siemens-healthineers.com>
2023-09-27 11:34:39 +0200

    add https server

--- cmd/server.go
+++ cmd/server.go
@@ -14,27 +14,55 @@ import (
 var memStats runtime.MemStats
 
 func main() {
-	var intPort int
+	var intHttpPort int
+	var intHttpsPort int
 	var strPath string
 	var strHost string
+	var strTLSCert string
+	var strTLSKey string
 
 	// Read the commandline and environment variables into the application config
 	var flags = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	flags.IntVar(&intPort, "port", 80, "port the server is listening on")
+	flags.IntVar(&intHttpPort, "http-port", 80, "http-port the server is listening on")
+	flags.IntVar(&intHttpsPort, "https-port", 443, "https-port the server is listening on")
 	flags.StringVar(&strPath, "path", "", "path the server is serving")
 	flags.StringVar(&strHost, "host", "", "host the server is serving")
+	flags.StringVar(&strTLSCert, "tls-cert", "", "certificate file location for the tls communication")
+	flags.StringVar(&strTLSKey, "tls-key", "", "public key for TLS communication")
 	flags.Parse(os.Args[1:])
 	flagutil.SetFlagsFromEnv(flags, "SERVER")
 
-	strPort := fmt.Sprintf("%s:%d", strHost, intPort)
+	strHttpAddr := fmt.Sprintf("%s:%d", strHost, intHttpPort)
+	strHttpsAddr := fmt.Sprintf("%s:%d", strHost, intHttpsPort)
 	strHandlerPath := fmt.Sprintf("%s/health", strPath)
 	runtime.ReadMemStats(&memStats)
 
-	mux := http.NewServeMux()
+	http.HandleFunc(strHandlerPath, handleHealth)
 
-	mux.HandleFunc(strHandlerPath, handleHealth)
-	klog.Infof("Listening on %s%s - MemoryAllocated: %d\n", strPort, strHandlerPath, memStats.TotalAlloc)
-	klog.Fatal(http.ListenAndServe(strPort, mux))
+	// create the http and https server
+	httpServer := &http.Server{
+		Addr: strHttpAddr,
+	}
+
+	httpsServer := &http.Server{
+		Addr: strHttpsAddr,
+	}
+
+	finish := make(chan bool)
+
+	go func() {
+		klog.Infof("Listening on %s%s - MemoryAllocated: %d\n", strHttpAddr, strHandlerPath, memStats.TotalAlloc)
+		klog.Fatal(httpServer.ListenAndServe())
+	}()
+
+	go func() {
+		if strTLSCert != "" && strTLSKey != "" {
+			klog.Infof("Listening on %s%s - MemoryAllocated: %d\n", strHttpsAddr, strHandlerPath, memStats.TotalAlloc)
+			klog.Fatal(httpsServer.ListenAndServeTLS(strTLSCert, strTLSKey))
+		}
+	}()
+
+	<-finish
 }
 
 func handleHealth(w http.ResponseWriter, r *http.Request) {
