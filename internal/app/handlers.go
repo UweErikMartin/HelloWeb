87fd2e21fc9d55443ac145307dd5bf74687bc645
Uwe-Erik Martin <uwe_erik_martin@yahoo.de>
2023-10-03 11:54:59 +0200

    put some message into health endpoint

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -1,7 +1,9 @@
 package application
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"k8s.io/klog"
 )
@@ -9,4 +11,6 @@ import (
 func (app *Application) Health(w http.ResponseWriter, r *http.Request) {
 	klog.Infoln("Serving health endpoint")
 	w.WriteHeader(http.StatusOK)
+	hn, _ := os.Hostname()
+	fmt.Fprintf(w, "Host: %s - health endpoint called from %s\n", hn, r.RemoteAddr)
 }
